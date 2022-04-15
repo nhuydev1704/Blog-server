5d8725888aeb2863b443be8a8eee250082d0eb6e
nguyennhuy1704 <y.nguyen@vinaseco.vn>
2022-04-15 10:22:43 +0700

    add origin server

--- server/index.ts
+++ server/index.ts
@@ -1,47 +1,48 @@
-import dotenv from 'dotenv'
-dotenv.config()
+import dotenv from "dotenv";
+dotenv.config();
 
-import express from 'express'
-import morgan from 'morgan'
-import cors from 'cors'
-import cookieParser from 'cookie-parser'
-import routes from './routes'
-import { createServer } from 'http'
-import { Server, Socket } from 'socket.io'
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import routes from "./routes";
+import { createServer } from "http";
+import { Server, Socket } from "socket.io";
 
-const app = express()
+const app = express();
 
 // Middleware
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-app.use(cors({
-    origin: `${process.env.BASE_URL}`,
-    credentials: true
-}))
-app.use(morgan('dev'))
-app.use(cookieParser())
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(
+  cors({
+    origin: `*`,
+    credentials: true,
+  })
+);
+app.use(morgan("dev"));
+app.use(cookieParser());
 
 // socket io
-const http = createServer(app)
+const http = createServer(app);
 export const io = new Server(http, {
-    cors: {
-        origin: `${process.env.BASE_URL}`,
-        credentials: true
-    }
-})
-import { SocketServer } from './config/socket'
+  cors: {
+    origin: `${process.env.BASE_URL}`,
+    credentials: true,
+  },
+});
+import { SocketServer } from "./config/socket";
 
-io.on("connection", (socket: Socket) => SocketServer(socket))
+io.on("connection", (socket: Socket) => SocketServer(socket));
 
 // Routes
-app.use('/api', routes)
+app.use("/api", routes);
 
 // database
-import './config/database'
-
+import "./config/database";
 
 // server port
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 http.listen(PORT, () => {
-    console.log("server runing: ", PORT)
-})
+  console.log("server runing: ", PORT);
+});
