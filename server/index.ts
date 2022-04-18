5bf23f0c167cbc95c46e36a8325cbada148cd90d
nguyennhuy1704 <y.nguyen@vinaseco.vn>
2022-04-18 23:46:53 +0700

    fix origin

--- server/index.ts
+++ server/index.ts
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(
   cors({
-    origin: `*`,
+    origin: `https://nhuydev.tk`,
     credentials: true,
   })
 );
